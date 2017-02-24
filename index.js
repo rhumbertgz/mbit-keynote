6ea34cb88ad9944f5f84811764e8534ef2e2aa86
Humberto Rodríguez Avila <rhumbertgz@gmail.com>
2017-02-24 19:46:56 +0100

    Update after JSHint suggestions

--- index.js
+++ index.js
@@ -9,33 +9,33 @@
 * 
 */
 
-var BBCMicrobit = require('bbc-microbit')
-var KeynoteAPI = require("./keynote")
-var keynote = new KeynoteAPI()
+var BBCMicrobit = require('bbc-microbit');
+var KeynoteAPI = require("./keynote");
+var keynote = new KeynoteAPI();
 
-console.log('Scanning for microbit...')
+console.log('Scanning for microbit...');
 BBCMicrobit.discover(function(microbit) {
   console.log('Discovered microbit: %s', microbit.address);
   
   microbit.on('disconnect', function() {
-    console.log('Microbit disconnected!')
+    console.log('Microbit disconnected!');
     process.exit(0);
   });
 
   microbit.on('buttonAChange', function(value) {
-    if(value == 1) keynote.prev()
+    if(value == 1) keynote.prev();
   });
 
   microbit.on('buttonBChange', function(value) {
-    if(value == 1) keynote.startOrNext()
+    if(value == 1) keynote.startOrNext();
   });
 
   console.log('Connecting microbit...');
   microbit.connectAndSetUp(function() {
-    console.log('Status: Connected')
-    console.log('Initializing...')
-    microbit.subscribeButtons(function(){})
-    console.log('Ready!')
-    microbit.writeLedText('Ready!', function(){})
+    console.log('Status: Connected');
+    console.log('Initializing...');
+    microbit.subscribeButtons(function(){});
+    console.log('Ready!');
+    microbit.writeLedText('Ready!', function(){});
   });
 });
--- keynote.js
+++ keynote.js
@@ -11,15 +11,14 @@ var Keynote = function () {};
 
 Keynote.prototype.startOrNext = function () { 
    osascript.execFile('./scripts/startOrNext.scpt', function(err, result){
-    if (err) return console.error(err)}
-)};
+    if (err) return console.error(err);});
+};
 
 Keynote.prototype.prev = function () { 
     osascript.execFile('./scripts/prev.scpt', function(err, result){
-    if (err) return console.error(err)}
-)};
+    if (err) return console.error(err);});
+};
 
 /*  export the API  */
 module.exports = Keynote;
-
 
